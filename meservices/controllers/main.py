879cdedbf1b08ca1fa10b78f4dac3e921f5d0cef
Valentin Thirion <vt@abakusitsolutions.eu>
2019-04-24 22:25:27 +0200

    [CHG] fixed csrf problem return of the form ok date management ok improved email

--- meservices/controllers/main.py
+++ meservices/controllers/main.py
@@ -3,13 +3,14 @@
 import logging
 from openerp import http, _
 from openerp.http import request
+from datetime import datetime
 
 _logger = logging.getLogger(__name__)
  
 class WebsiteMEServicesContactForm(http.Controller):
 
-    @http.route(['/contactform/sent'], type='http', auth="public", methods=['POST'], website=True)
-    def contacted_form(self, redirect=None, **post):
+    @http.route(['/contactform'], type='http', auth='public', methods=['POST'], website=True, csrf=False)
+    def contacted_form(self, **post):
         if post:
             # Partner
             partner_id = request.env['res.partner'].sudo().create({
@@ -33,12 +34,14 @@ class WebsiteMEServicesContactForm(http.Controller):
                 line_description = _('Insect type: ') + post['insect_type']
                 line_description += _('\nNest location: ') + post['nest_location']
                 line_description += _('\nDetails: ') + post['details']
+                already_came_date = ''
                 if 'already_came' in post:
-                    line_description += _('\nAlready came on: ') + post['already_came']
+                    already_came_date = datetime.strptime(post['already_came_date'], '%Y-%m-%d')
+                    line_description += _('\nAlready came on: ') + datetime.strftime(already_came_date, '%d/%m/%Y')
                 else:
                     post['already_came'] = 'False'
                 line_description += _('\nNotes: ') + post['description']
-
+    
                 sale_order_line_id = request.env['sale.order.line'].sudo().create({
                     'order_id': sale_order_id.id,
                     'product_id': product_id.id,
@@ -46,27 +49,26 @@ class WebsiteMEServicesContactForm(http.Controller):
                     'nest_location': post['nest_location'],
                     'situation_details': post['details'],
                     'already_came': post['already_came'],
-                    
+                    'already_came_date': already_came_date,
                     'notes': post['description'],
                     'name': line_description,
                 })
-                # 'already_came_date': post['already_came_date'],
+                # 
 
-            _logger.debug("\n\n Request: %s", request)
             #company = request.env['']
 
             email_desc = _("Hello,<br />A new request has been made on your website.<br />")
-            email_desc += _("<br />Nom: ") + post['contact_name']
-            email_desc += _("<br />Mobile: ") + post['mobile']
-            email_desc += _("<br />Address: ") + post['street'] + ", " + post['zip'] + " " + post['city']
+            email_desc += _("<br /><b><u>Name:</b></u> ") + post['contact_name']
+            email_desc += _("<br /><b><u>Mobile</b></u> ") + post['mobile']
+            email_desc += _("<br /><b><u>Address</b></u> ") + post['street'] + ", " + post['zip'] + " " + post['city']
             email_desc += _("<br />")
-            email_desc += _("<br />Info: <br />") + line_description.replace('\n', '<br />')
+            email_desc += _("<br /><b><u>Info</b></u> <br />") + line_description.replace('\n', '<br />')
             email_desc += _("<br /><br />Have a great day")
             base_url = request.env['ir.config_parameter'].sudo().get_param('web.base.url')
             url = "%s/web#id=%s&view_type=form&model=sale.order" % (base_url, sale_order_id.id)
             email_desc += _("<br />check it out here: <a href='") + url + "'>here</a>"
 
-            mail_id = request.env['mail.mail'].create({
+            mail_id = request.env['mail.mail'].sudo().create({
                 'body_html': email_desc,
                 'subject': 'Website: New demand',
                 'email_to': 'valentinthirion@gmail.com',
@@ -75,6 +77,8 @@ class WebsiteMEServicesContactForm(http.Controller):
                 'type': 'email',
                 'auto_delete': False,
             })
-            request.env['mail.mail'].send([mail_id])
+            request.env['mail.mail'].sudo().send([mail_id])
+
+            return request.render('meservices.contact_form_sent', {'result': 'OK'})
         else:
             return request.redirect("/")
